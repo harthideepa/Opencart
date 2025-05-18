a88ffe0f668ed8b62e3cc70eab41a3e9e69d4dbc
HarthiDeepa <harthideepa@gmail.com>
2025-05-18 23:27:38 +0530

    Sarch Product

--- Utils/Search.js
+++ Utils/Search.js
@@ -15,6 +15,30 @@ class Search
         
         await this.searchLocator.search(productName)
     }
+
+    async SearchInvalid(productName){
+        await this.searchLocator.InvalidProductName(productName)
+    }
+    async searchProductCriteria(name)
+    {
+        await this.searchLocator.searchCriteria(name)
+    }
+
+    async verifyMacPage()
+    {
+        await this.searchLocator.getIntoMac()
+    }
+
+    async CheckWithCategories(productName,option)
+    {
+        await this.searchLocator.categoriesOption(productName,option)
+    }
+
+    async sort(sortingOrder)
+    {
+        await this.searchLocator.searchWithSortByOption(sortingOrder)
+        await this.searchLocator.VerifySortByOptionResult(sortingOrder)
+    }
 }
 
 module.exports = Search
